f987d50e62f69c5e40ef667e62be1a7699696f9f
autom8ter <admin@autom8ter.com>
2019-03-12 20:34:12 -0600

    db

--- db.go
+++ db.go
@@ -29,7 +29,6 @@ type Person struct {
 package gonet
 
 import (
-	"github.com/autom8ter/gonet/driver"
 	"github.com/go-bongo/bongo"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
@@ -37,10 +36,11 @@ import (
 )
 
 type MongoDB struct {
+	collection string
 	conn *bongo.Connection
 }
 
-func NewMongoDB(connectionStr, database string) *MongoDB {
+func NewMongoDB(collName, connectionStr, database string) *MongoDB {
 	cfg := &bongo.Config{
 		ConnectionString: connectionStr,
 		Database:         database,
@@ -50,19 +50,22 @@ func NewMongoDB(connectionStr, database string) *MongoDB {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return &MongoDB{connection}
+	return &MongoDB{
+		collection: collName,
+		conn: connection,
+	}
 }
 
-func (m *MongoDB) GetConfig() *bongo.Config {
+func (m *MongoDB) Config() *bongo.Config {
 	return m.conn.Config
 }
 
-func (m *MongoDB) GetSession() *mgo.Session {
+func (m *MongoDB) Session() *mgo.Session {
 	return m.conn.Session
 }
 
-func (m *MongoDB) Collection(name string) *bongo.Collection {
-	return m.conn.Collection(name)
+func (m *MongoDB) Collection() *bongo.Collection {
+	return m.conn.Collection(m.collection)
 }
 
 func (m *MongoDB) Connect() error {
@@ -81,38 +84,34 @@ func (m *MongoDB) GetDB(name string) *mgo.Database {
 	return m.conn.Session.DB(name)
 }
 
-func (m *MongoDB) SaveDoc(collname string, obj bongo.Document) error {
-	return m.conn.Collection(collname).Save(obj)
+func (m *MongoDB) SaveDoc(obj bongo.Document) error {
+	return m.conn.Collection(m.collection).Save(obj)
 }
 
-func (m *MongoDB) PreSaveDoc(collname string, obj bongo.Document) error {
-	return m.conn.Collection(collname).PreSave(obj)
+func (m *MongoDB) PreSaveDoc(obj bongo.Document) error {
+	return m.conn.Collection(m.collection).PreSave(obj)
 }
 
-func (m *MongoDB) DeleteDoc(collname string, query bson.M) (*mgo.ChangeInfo, error) {
-	return m.conn.Collection(collname).Delete(query)
+func (m *MongoDB) DeleteDoc(query bson.M) (*mgo.ChangeInfo, error) {
+	return m.conn.Collection(m.collection).Delete(query)
 }
 
-func (m *MongoDB) FindOne(collname string, query interface{}, doc interface{}) error {
-	return m.conn.Collection(collname).FindOne(query, doc)
+func (m *MongoDB) FindOne(query interface{}, doc interface{}) error {
+	return m.conn.Collection(m.collection).FindOne(query, doc)
 }
 
-func (m *MongoDB) FindById(collname string, id bson.ObjectId, doc interface{}) error {
-	return m.conn.Collection(collname).FindById(id, doc)
+func (m *MongoDB) FindById(id bson.ObjectId, doc interface{}) error {
+	return m.conn.Collection(m.collection).FindById(id, doc)
 }
 
-func (m *MongoDB) Find(collname string, query interface{}) *bongo.ResultSet {
-	return m.conn.Collection(collname).Find(query)
+func (m *MongoDB) Find(query interface{}) *bongo.ResultSet {
+	return m.conn.Collection(m.collection).Find(query)
 }
 
-func (m *MongoDB) DeleteOne(collname string, query bson.M) error {
-	return m.conn.Collection(collname).DeleteOne(query)
+func (m *MongoDB) DeleteOne(query bson.M) error {
+	return m.conn.Collection(m.collection).DeleteOne(query)
 }
 
-func (m *MongoDB) DeleteDocument(collname string, doc bongo.Document) error {
-	return m.conn.Collection(collname).DeleteDocument(doc)
-}
-
-func (m *MongoDB) AsDB() driver.DB {
-	return m
+func (m *MongoDB) DeleteDocument(doc bongo.Document) error {
+	return m.conn.Collection(m.collection).DeleteDocument(doc)
 }
